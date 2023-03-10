2be6bfe1d40ba4fe6c57714696aebe2d30816eee
ginnarodriguez <grodriguez47@gmail.com>
2023-03-09 22:23:57 -0500

    practicando

--- variables.js
+++ variables.js
@@ -1,8 +1,12 @@
-//palabra constante no cambia en el tiempo
+// const palabra constante no cambia en el tiempo
 const valorPasaje = 1650;
 
 console.log(valorPasaje);
 const nombrePasajero = "Camilo";
 const apellidoPasajero = "Figueroa";
-nombreCompleto = nombrePasajero + " " + apellidoPasajero;
+
+
+//let puede cambiar en el tiempo se aplica a lo que contenga el bloque
+let nombreCompleto = nombrePasajero + " " + apellidoPasajero;
 console.log(nombreCompleto);
+ //var Puede cambiar durante el tiempo y es aplicable a todo el documento
