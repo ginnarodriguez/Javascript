5dd440ca6d9ec741fe752b7fc0200cf5d0a4c2fb
ginnarodriguez <grodriguez47@gmail.com>
2023-03-09 22:08:40 -0500

    ejercicios

---
+++ variables.js
@@ -0,0 +1,8 @@
+//palabra constante no cambia en el tiempo
+const valorPasaje = 1650;
+
+console.log(valorPasaje);
+const nombrePasajero = "Camilo";
+const apellidoPasajero = "Figueroa";
+nombreCompleto = nombrePasajero + " " + apellidoPasajero;
+console.log(nombreCompleto);
